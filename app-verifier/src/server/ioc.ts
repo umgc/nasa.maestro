4598da446d8d22762dc2014b01b490b0134797aa
Alberto Bonfiglio <alberto.bonfiglio@gmail.com>
2020-04-25 14:29:53 -0700

    (fix) made image comparison more sensitive

--- app-verifier/src/server/ioc.ts
+++ app-verifier/src/server/ioc.ts
@@ -3,7 +3,7 @@ import awilix, { AwilixContainer } from 'awilix';
 import CheckerService from './checker-service.js';
 import DocXValidatorService from './docx-validator.js';
 import spawn from 'child_process';
-import PDFImage from 'pdf-image';
+//import PDFImage from 'pdf-image';
 import Common from './common.js';
 import Unoconv from 'unoconv-promise';
 
@@ -19,7 +19,7 @@ export default class IOC {
       common: awilix.asClass(Common),
       checkService: awilix.asClass(CheckerService),
       docXService: awilix.asClass(DocXValidatorService),
-      pdf2imageService: awilix.asValue(PDFImage),
+      //pdf2imageService: awilix.asValue(PDFImage),
       unoconv: awilix.asValue(Unoconv),
       spawn: awilix.asValue(spawn),
     });
